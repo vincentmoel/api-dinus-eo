c3b41a873b0932a8d53ecf551bddede992262565
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2022-06-22 22:53:15 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            BuildingSeeder::class,
-            RoomSeeder::class,
+            // BuildingSeeder::class,
+            // RoomSeeder::class,
             // EventSeeder::class,
         ]);
     }
